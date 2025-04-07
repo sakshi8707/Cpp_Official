e4ce6729f253c2f0a1015f04a5de3b9598922a29
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2025-04-07 14:52:23 +0530

    Update cycle_detection_in_Directed_graph.cpp

--- Graph/cycle_detection_in_Directed_graph.cpp
+++ Graph/cycle_detection_in_Directed_graph.cpp
@@ -1,2 +1,50 @@
-#include<std/c++.h>
-using namespace std;
+
+
+import java.util.*;
+
+class Node{
+    int first;
+    int second;
+    public Node(int first , int second){
+        this.first = first;
+        this.second = second;   
+    }
+}
+
+public class cyclePresent{
+
+    static boolean checkForCycle(int node , boolean[] vis , ArrayList<ArrayList<Integer>> adj, int[] parent)
+    {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(s,-1));
+        vis[node] = true;
+
+        while(!q.isEmpty())
+        {
+           int newNode = q.peek().first;
+           int newParent = q.peek().second;
+           q.remove();
+
+           for(Integer it : adj.get(newNode))
+           {
+            if(vis[it]==false)
+            {
+
+            q.add(new Node(it,newNode));
+            vis[it] = true;
+            }
+            else if(newParent==it)
+            {
+                    return true;
+            }
+           }
+           return false;    
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner();
+    }
+}
