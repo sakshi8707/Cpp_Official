6f9a5967c4011f86dfd2dbdf6924a288ee6350c1
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2025-09-09 13:06:27 +0530

    Added soution class

--- LINKED LIST/cycle_detection_in_linked_list.cpp
+++ LINKED LIST/cycle_detection_in_linked_list.cpp
@@ -13,7 +13,30 @@ class node
     }
 };
   // function for insertat tail node
-    
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* s= head,*f = head;
+        while(f!=NULL&&f->next!=NULL)
+        {
+            s= s->next;
+            f= f->next->next;
+            if(f==s)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
     
 
 int main()
